57d0877e5458d0e0b37fe80bda71b98e815b50e3
Michael Kevin <michaelkmb@hotmail.com>
2022-04-17 09:26:14 -0300

    modal done;

--- alura_acessibilidade/js/dialog.js
+++ alura_acessibilidade/js/dialog.js
@@ -3,17 +3,36 @@ var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+var dialogInert = document.querySelector('#conteudoForaDialog');
+var video = document.querySelector('.video');
 
+btnAbreDialog.style.display = 'block';
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus();
+  video.removeAttribute('controls');
+  dialogInert.inert = true;
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto');
+  dialogInert.inert = false;
+  video.setAttribute('controls', true);
+  btnAbreDialog.focus();
 }
 
 // Listeners
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+
+document.onkeydown = function(evt) {
+  evt = evt || window.event;
+  var isEscape = false;
+  if ("key" in evt) 
+    isEscape = (evt.key === "Escape" || evt.key === "Esc");
+  
+  if(isEscape && dialog.classList.contains('dialogNewsletter--aberto'))
+    fechandoDialog();
+}
