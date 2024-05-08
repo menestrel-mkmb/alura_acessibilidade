d3e93dd0dd531e28ed8cfcabb0fb721a052a155a
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-05-08 15:53:35 -0300

    misc: ruler follow cursor to guide ruler;

--- alura_acessibilidade/js/ruler.js
+++ alura_acessibilidade/js/ruler.js
@@ -5,6 +5,24 @@ const rulerBefore = document.createElement('div');
 const rulerAfter = document.createElement('div');
 const ruler = document.createElement('div');
 
+document.addEventListener('mousemove', (e) => followCursorHorizontal(e));
+
+function followCursorHorizontal (e) {
+    rulerSection.style.flexDirection = 'column';
+
+    rulerBefore.style.height = Number(e.clientY - ruler.offsetHeight/2) + 'px';
+    rulerAfter.style.height = (window.innerHeight - e.clientY - ruler.offsetHeight/2) + 'px';
+}
+
+// A título de teste de modularidade e nome de classes
+// document.addEventListener('mousemove', (e) => followCursorVertical(e));
+// function followCursorVertical (e) {
+//     rulerSection.style.flexDirection = 'row';
+
+//     rulerBefore.style.width = Number(e.clientX - ruler.offsetWidth/2) + 'px';
+//     rulerAfter.style.width = (window.innerWidth - e.clientX - ruler.offsetWidth/2) + 'px';
+// }
+
 (function createFocusRuler () {
     rulerSection.classList.add('ruler__sect');
 
